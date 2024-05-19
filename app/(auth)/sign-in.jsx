48cdf838772e09cb1159dccacc4eb2b2c87ddb03
Akgisno1 <anshulkumargodiwar2001@gmail.com>
2024-05-19 18:05:15 +0530

    sign in up appwrite context

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,88 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View, Alert } from "react-native";
 import React from "react";
 
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import FormField from "../../components/FormField";
+import { useState } from "react";
+import CustomButton from "../../components/CustomButton";
+import { Link, router } from "expo-router";
+import { getCurrentUser, signIn } from "../../lib/appwrite";
+import { useGlobalContext } from "../../context/GlobalProvider";
+
 const Signin = () => {
+  const [form, setform] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [isSubmitting, setisSubmitting] = useState(false);
+  const { setuser, user, isLoggedin, setisLoggedIn } = useGlobalContext;
+  const submit = async () => {
+    if (form.email === "" || form.password === "") {
+      Alert.alert("Error", "Please fill in all fields");
+    }
+    setisSubmitting(true);
+    try {
+      await signIn(form.email, form.password);
+      const result = await getCurrentUser();
+      setuser(result);
+
+      setisLoggedIn(true);
+      Alert.alert("Success", "You have successfully logged in");
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setisSubmitting(false);
+    }
+  };
+
   return (
-    <View>
-      <Text>Signin</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-[85vh] px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
+          <Text className="text-2xl text-white mt-10 font-psemibold text-semibold">
+            Log in to Aora
+          </Text>
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setform({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setform({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+          <CustomButton
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have account?
+            </Text>
+            <Link
+              href="/sign-up"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Sign Up
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
